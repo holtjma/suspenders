7c773f1f1cbeaa5ebfdff68c3ea39097f0f6f0f5
James Matthew Holt <holtjma@cs.unc.edu>
2013-02-27 15:28:18 -0500

    Updating setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='suspenders',
       version='0.1',
       description='Allows the merging of alignments that have been annotated using pylapels into a single alignment that picks the highest quality alignment.',
-      url='<INSERT URL>',
+      url='http://code.google.com/p/suspenders',
       author='James Holt',
       author_email='holtjma@cs.unc.edu',
       license='MIT',
